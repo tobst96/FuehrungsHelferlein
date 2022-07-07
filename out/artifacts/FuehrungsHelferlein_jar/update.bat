d865b3fa5edd3a641eb55ccc3344b76945b41218
tobst96 <52480593+tobst96@users.noreply.github.com>
2022-07-07 19:40:24 +0200

    v0.0.2

--- out/artifacts/FuehrungsHelferlein_jar/update.bat
+++ out/artifacts/FuehrungsHelferlein_jar/update.bat
@@ -1,4 +1,5 @@
 @echo off
 title Update
+del FuehrungsHelferlein.jar
 curl -o FuehrungsHelferlein.jar https://github.com/tobst96/FuehrungsHelferlein/blob/main/out/artifacts/FuehrungsHelferlein_jar/FuehrungsHelferlein.jar
 pause
