ee80e56a85b7656363084453db61786b631ce0c5
tobst96 <52480593+tobst96@users.noreply.github.com>
2022-07-07 19:47:47 +0200

    Update Zerstört das Projekt, somit erstmal entfernt

--- out/artifacts/FuehrungsHelferlein_jar/update.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Update
-del FuehrungsHelferlein.jar
-curl -o FuehrungsHelferlein.jar https://github.com/tobst96/FuehrungsHelferlein/blob/main/out/artifacts/FuehrungsHelferlein_jar/FuehrungsHelferlein.jar
-pause
