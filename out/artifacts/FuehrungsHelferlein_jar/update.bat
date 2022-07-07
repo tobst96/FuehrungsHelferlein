871e9811f0199f8a3037deace51c1ad95dac3470
tobst96 <52480593+tobst96@users.noreply.github.com>
2022-07-07 18:55:08 +0200

    Create Update Datei

---
+++ out/artifacts/FuehrungsHelferlein_jar/update.bat
@@ -0,0 +1,4 @@
+@echo off
+title Update
+curl -o FuehrungsHelferlein.jar https://github.com/tobst96/FuehrungsHelferlein/blob/main/out/artifacts/FuehrungsHelferlein_jar/FuehrungsHelferlein.jar
+pause
